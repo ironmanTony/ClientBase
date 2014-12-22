5c8479b58605dd6e57d0183faabf644c2a798bd2
李涛 <ironman@wosai-inc.com>
2014-12-22 16:14:25 +0800

    create commit

---
+++ app/src/main/java/com/me/client/framework/view/ActivityMain.java
@@ -0,0 +1,21 @@
+package com.me.client.framework.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.me.client.R;
+
+
+public class ActivityMain extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
