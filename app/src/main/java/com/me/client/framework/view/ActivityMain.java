fbc08f586443da271354fefbdd76284d4b7ecf4c
ironman.li <ironman.li@qunar.com>
2015-05-03 13:53:04 +0800

    新建clientbaseModel

--- app/src/main/java/com/me/client/framework/view/ActivityMain.java
+++ app/src/main/java/com/me/client/framework/view/ActivityMain.java
@@ -1,14 +1,8 @@
 package com.me.client.framework.view;
 
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.me.client.R;
 
 
@@ -22,18 +16,6 @@ public class ActivityMain extends ActionBarActivity {
     }
 
     private void initView(){
-        SlidingMenu menu = new SlidingMenu(this);
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.color.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        menu.setMenu(R.layout.menu);
-//        DrawerLayout layout = (DrawerLayout)findViewById(R.id.drawer_layout);
-//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.drawer_container);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 
 
